8826bb9b573d56170bee2f33354f231a281c3824
Orhan Yilmaz 0291000189 <orhan.yilmaz@zte.com.cn>
2022-01-26 19:58:25 +0300

    Arrays

--- main.go
+++ main.go
@@ -5,20 +5,36 @@ import "fmt"
 func main() {
 	conferenceName := "Go Conference"
 	conferenceTickets := 50
-	var remainingTickets int = 30
+	var remainingTickets uint = 50
+	var bookings [50]string
 
 	fmt.Printf("conferenceName %T, conferenceTickets is %T, remainingTickets is %T", conferenceName, conferenceTickets, remainingTickets)
-
 	fmt.Printf("Welcome to %v booking application\n", conferenceName)
 	fmt.Println("We have total of", conferenceTickets, "tickets and", remainingTickets, "are still available")
 	fmt.Println("Get your tickets here to attend")
 
-	var userName string
-	var userTickets int
-	// ask user for their name
+	var firstName string
+	var lastName string
+	var email string
+	var userTickets uint
 
-	userName = "Tom"
-	userTickets = 2
-	fmt.Printf("User %v booked %v tickets. \n", userName, userTickets)
+	fmt.Println("Enter your first name : ")
+	fmt.Scan(&firstName)
+	fmt.Println("Enter your lsat name : ")
+	fmt.Scan(&lastName)
+	fmt.Println("Enter your email address : ")
+	fmt.Scan(&email)
+	fmt.Println("Enter number of tickets : ")
+	fmt.Scan(&userTickets)
 
+	remainingTickets = remainingTickets - userTickets
+	bookings[0] = firstName + " " + lastName
+
+	fmt.Printf("The whole array: %v\n", bookings)
+	fmt.Printf("The first value : %v\n", bookings[0])
+	fmt.Printf("Array Type: %T\n", bookings)
+	fmt.Printf("Array Length : %v\n", len(bookings))
+
+	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
+	fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
 }
